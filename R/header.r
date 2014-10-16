3d422ab3263018efc35d0fbaec956617e76e3405
Sinan Shi <shisinancn@gmail.com>
2014-10-16 09:28:18 +0200

    get regular grid

--- R/header.r
+++ R/header.r
@@ -4,7 +4,8 @@ library(raster)
 library(sp)
 library(ggplot2)
 
-
+source("/home/sinan/workspace/visotmed/R/getGrid.r")
+source("/home/sinan/workspace/visotmed/R/getRegular.r")
 source("/home/sinan/workspace/visotmed/R/map2spatial.r")
 source("/home/sinan/workspace/visotmed/R/mapConvert.r")
 source("/home/sinan/workspace/visotmed/R/mapConvert.nc.r") 
